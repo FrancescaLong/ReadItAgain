c3a35a7cfaccee7aa034a66cd4d1176d925705ec
FrancescaLong <francesca_long@hotmail.com>
2015-12-09 23:36:04 -0800

    initial commit for cloned project

---
+++ README.md
@@ -0,0 +1,5 @@
+// To start this app or work on it...
+
+1. run npm install -g http-server or sudo npm install http-server
+2. in the terminal, enter    http-server
+3. open a browser window and go to local-host:8080
---
+++ app.js
@@ -0,0 +1,101 @@
+var app = angular.module('angularSmall', ['ui.router', 'ngResource']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+		.state('user-index', {
+			url: "/",
+			templateUrl: "templates/user-index.html",
+			controller: 'UserIndexCtrl'
+		})
+		.state('user-show', {
+			url: "/users/:user_id",
+			templateUrl: "templates/user-show.html",
+			controller: 'UserShowCtrl'
+		});
+
+
+	// For any unmatched URLs, redirect to
+	$urlRouterProvider.otherwise("/");
+
+	});
+
+
+// CONTROLLERS
+app.controller('MainCtrl', function ($scope) {
+	$scope.message = "World!";
+});
+
+
+app.controller('UserIndexCtrl', function ($scope, User) {
+	$scope.users = User.query();
+
+	$scope.addUser = function() {
+		User.save($scope.newUser, function (data) {
+			$scope.users.push(data);
+			$scope.newUser = {};
+		})
+	};
+
+	$scope.removeUser = function(user) {
+		User.remove(user, function (data) {
+			var index = $scope.users.indexOf(user);
+			$scope.users.splice(index, 1);
+		});
+	}
+});
+
+app.controller('UserShowCtrl', function ($stateParams, $scope, User, $http) {
+	User.get({ id: $stateParams.user_id }, function (data) {
+		$scope.user = data;
+		getGifs(data.firstname)
+	});
+
+	function getGifs(firstName) {
+		var query = 'http://api.giphy.com/v1/gifs/search?q=' + firstName + '&api_key=dc6zaTOxFJmzC';
+
+		$http.get(query).then(
+			function (data) {
+				$scope.gifs = data.data.data
+			},
+			function (error) {
+				console.log(error);
+			}
+		);
+	}
+
+});
+
+app.factory('User', function ($resource) {
+	var HOST = 'http://daretodiscover.herokuapp.com'
+	return $resource(HOST + '/users/:id', { id: '@id' })
+});
+
+
+/*	var users = [
+		{ id: 1, name: 'Betsy', age: 73 },
+		{ id: 2, name: 'Norman', age: 54 },
+		{ id: 3, name: 'Sonya', age: 6 }
+	];
+
+	// User.all() => all users
+	// User.remove() => remove a user
+	// User.get(user_id) => get a user
+	return {
+		all: function () {
+			return users;
+		},
+		remove: function(user) {
+			users.splice(users.indexOf(user), 1);
+		},
+		get: function(userId) {
+			for (var i = 0; i < users.length; i++); {
+				if (users[i].id === parseInt(userId)) {
+					return users[i];
+				}
+			}
+		return null;
+		}
+	};
+})
+*/
